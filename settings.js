02c2b63f2a2893194453a6f065c1b6fcc75dea76
Crkychiki <jasonclericuzio@gmail.com>
2024-06-02 12:50:58 +0700

    Update settings.js
    Ok

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "7498426149:AAF-HKc-9031bwoq9ra0Mj-GhFDMNXVkWfI" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Crysky_botz" //your bot name
+global.BOT_TOKEN = "7426536539:AAGq_d2tIM4zZQilsAWjhdfl7SKnU1DqBvg" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "rpg_bot" //your bot name
 global.OWNER_NAME = "Crysky" //your name
 global.OWNER_NUMBER = "6287838882011" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER = ["https://t.me/Crysky_own", "https://t.me/Crysky_own"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
