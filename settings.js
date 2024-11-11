9e859957a0ecab39af47067b1a79a9defe68ef51
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-11 07:22:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,23 +5,17 @@ const {
 
 // Website Api (jgn di ganti biar gk eror)
 global.APIs = {
-   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
-}
-
-//buy apikey premium 0887435047326
-// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
-global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   nstapi: 'https://api.neastooid.xyz', //apabila link api eror, segera laporkan ke owner
 }
 
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
 global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
+global.BOT_NAME = "LinucxMD Telegram Botz" //your bot name
+global.OWNER_NAME = "NeofetchNpc" //your name
 global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER = ["https://t.me/zeeoneofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
